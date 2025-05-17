2299a3814e3c37a3793855d99c8006c92938b078
Puneet <puneet.s@applify.co>
2025-05-17 17:12:19 +0530

    Add product image to upload folder

---
+++ src/main/java/com/m8/shopping/service/photo/PhotoService.java
@@ -0,0 +1,35 @@
+package com.m8.shopping.service.photo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.m8.shopping.model.photo.Photo;
+import com.m8.shopping.repository.photo.PhotoRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PhotoService {
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    public Photo createPhoto(Photo photo) {
+        return photoRepository.save(photo);
+    }
+
+    public Optional<Photo> getPhotoById(Long photoId) {
+        return photoRepository.findById(photoId);
+    }
+
+    public Photo updatePhoto(Photo photo) {
+        return photoRepository.save(photo);
+    }
+
+    public void deletePhoto(Long photoId) {
+        photoRepository.deleteById(photoId);
+    }
+
+    public List<Photo> getAllPhotos() {
+        return photoRepository.findAll();
+    }
+}
